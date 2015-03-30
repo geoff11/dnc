4cfe46cb3f12f8e89dc78450cbb99a79545ba625
geoff <geoff@antergeoff>
2015-03-30 14:19:31 +0200

    push

---
+++ chat.py
@@ -0,0 +1,11 @@
+
+class chat:
+    
+    '''
+    Classe chat : gère les différents messages du user
+    '''
+    
+    def __init__(self):
+        '''
+            Initialisation du chat
+        '''
