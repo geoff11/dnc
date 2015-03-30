5ae5d947d7d9b3a86714d0ad71b5ad54b0bc802a
geoff <geoff@antergeoff>
2015-03-30 13:51:05 +0200

    if serveur

--- serveur.py
+++ serveur.py
@@ -1,4 +1,3 @@
-__author__ = 'think'
 import sys.os.os.path
 from socket import *
 
@@ -9,18 +8,14 @@ from socket import *
 from datetime import *
 import sys
 
-if len(sys.argv) !=2 :
+if len(sys.argv) != 2 :
     print("Usage: {} <port>".format(sys.argv[0]))
     sys.exit(1)
 
-TAILLE_TAMPON=256
-# Tableaux de jours et mois pour affichage propre
-JOURS = ['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi']
-MOIS = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre',
-        'octobre', 'novembre', 'décembre'] 
+TAILLE_TAMPON = 256
 
 # Ouverture en ecriture du fichier de log
-logs = open("serveur_date.log", "w")
+logs = open("serveur.log", "w")
     
 maSock = socket(AF_INET,SOCK_DGRAM)
 maSock.bind(('',int(sys.argv[1])))
@@ -28,28 +23,53 @@ print("Serveur en attente sur le port {} .".format(sys.argv[1],), file=logs)  # 
 
 while True:
     try:
-        # Récupération de la requête du client
+        # Recuperation de la requete du client
         requete = maSock.recvfrom(TAILLE_TAMPON)
         
-        # Extraction du message et de l’adresse sur le client
+        # Extraction du message et de l adresse sur le client
         (mess, adr_client) = requete
         ip_client, port_client = adr_client
 
         message = mess.decode().lower()
         
         
-        print("Requête provenant de {}. Longueur = {}". \
+        print("Requete provenant de {}. Longueur = {}". \
         format(ip_client, len(mess)), file=logs)      # Ecriture du fichier de log 
         
-        # Construction de la réponse
-        reponse = "100 "
         
-        # Envoi de la réponse au client
+        # Construction de la reponse
+        
+        if message == "sleep":
+            reponse = self.sleep()
+        elif message == "list":
+            reponse = self.list()
+        elif message == "quit":
+            reponse = self.quit()
+        elif message == "wake":
+            reponse = self.wake()
+        elif message == "logchange":
+            reponse = self.logchange()
+        elif message == "private":
+            reponse = self.private()
+        elif message == "acceptpc":
+            reponse = self.acceptpc()
+        elif message == "denypc":
+            reponse = self.denypc()
+        elif message == "stoppc":
+            reponse = self.stoppc()
+        elif message == "filesend":
+            reponse = self.filesend()
+        elif message == "fileacc":
+            reponse = self.fileacc()
+        elif message == "fileden":
+            reponse = self.fileden()
+        
+        # Envoi de la reponse au client
         maSock.sendto(reponse.encode(), adr_client)
         
     except KeyboardInterrupt: break
     
 maSock.close()
-print("Arrêt du serveur", file=logs)
+print("Arret du serveur", file=logs)
 logs.close() 
 sys.exit(0)
